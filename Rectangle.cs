0e1c86a9b60753e305e4d771717f0bbb90457297
Roméo <giorgio.romeo95@gmail.com>
2023-11-12 20:37:11 +0100

    Méthodes
    Perimetre
    Aire
    EstCarre
    AfficherRectangle

--- Rectangle.cs
+++ Rectangle.cs
@@ -8,7 +8,7 @@ namespace Rectangle
 {
     internal class Rectangle
     {
-        // Attributs
+        /// Attributs
         private int longueur;
         private int largeur;
 
@@ -39,6 +39,38 @@ namespace Rectangle
         {
             this.longueur = longueur;
             this.largeur = largeur;
+        }
+
+        /// <summary>
+        /// Calcule le perimetre du rectangle
+        /// </summary>
+        /// <returns>Le perimetre calculé</returns>
+        public int Perimetre ()
+        {
+            return 2*(longueur+largeur);
+        }
+
+        /// <summary>
+        /// Calcule l'aire du rectangle
+        /// </summary>
+        /// <returns>L'aire calculée</returns>
+        public int Aire()
+        {
+            return longueur * largeur;
+        }
+
+        /// <summary>
+        /// Détermine si le rectangle est un carré
+        /// </summary>
+        /// <returns>true si le rectangle est un carré</returns>
+        public bool EstCarre()
+        {
+            return longueur == largeur;
+        }
+
+        public void AfficherRectangle()
+        {
+            Console.WriteLine("Longueur: {0} - Largeur: {1} - Perimetre: {2} - Aire: {3} - {4}", longueur, largeur, Perimetre(), Aire(), EstCarre() ? "C'est un carré" : "Ce n'est pas un carré");
         }
     }
 }
