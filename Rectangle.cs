3d9054ef9115ef264a6d777954e117782c8e6027
Roméo <giorgio.romeo95@gmail.com>
2023-11-12 20:11:07 +0100

    Ajout des attributs

---
+++ Rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    internal class Rectangle
+    {
+        // Attributs
+        private int longueur;
+        private int largeur;
+
+
+    }
+}
